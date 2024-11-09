b39f282100b794a6e19301dc3e96019797c08e78
Martin Kaliman <martinkaliman120@gmail.com>
2024-11-10 00:23:43 +0100

    Deleted app.css, index.css, modified app.jsx and main.jsx, completed exercises 1.1 and 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props of React",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ]
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course = {course}/>
+      <Content parts = {parts}/>
+      <Total total = {parts.reduce((sum, part) => sum + part.exercises, 0)} />
+    </div>
   )
 }
+
+const Header = ({course}) => <h1>{course}</h1>
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part part = {parts[0]}/>
+      <Part part = {parts[1]}/>
+      <Part part = {parts[2]}/>
+    </div>
+  )
+}
+
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+
+const Total = ({total}) => <p>Total exercises: {total}</p>
 
 export default App
